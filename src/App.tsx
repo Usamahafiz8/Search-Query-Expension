ae3d4c7b68c9e02ec22c3c77123e8a9462e42146
usamahafiz8 <usamahafiz8@gmail.com>
2023-06-19 23:25:51 +0500

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import { QueryResults } from "./components/queryResults";
 
 function App() {
   return (
-    <div >
-      {/* <header className="App-header"> */}
-        <h2 style={{ textTransform: "uppercase" , textAlign:"center", fontSize:"36px"}}>
-          Query Expansion for code search
-        </h2>
-      {/* </header> */}
-      <div style={{ padding: "20px" }}>
+    <div>
+      <h2
+        style={{
+          textTransform: "uppercase",
+          textAlign: "center",
+          fontSize: "36px",
+          color:" #AB7442"
+        }}
+      >
+        Query Expansion for code search
+      </h2>
+      <div style={{ padding: "24px" }}>
         <QueryResults />
       </div>
-      <footer className="App-footer">
-        <CopyRightMArk />
-      </footer>
     </div>
   );
 }
