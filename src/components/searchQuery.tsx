b6f9a0ef9282b0887f69bec3cbe9cadd261291a3
usamahafiz8 <usamahafiz8@gmail.com>
2023-06-19 22:02:14 +0500

    resolve issue

--- src/components/searchQuery.tsx
+++ src/components/searchQuery.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete, {
   AutocompleteInputChangeReason,
 } from "@mui/material/Autocomplete";
 import { top100Films } from "../content/seachResultRelated";
+
 
 export const QuerySearch: React.FC = () => {
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
@@ -22,9 +23,8 @@ export const QuerySearch: React.FC = () => {
       const selectedFilm = top100Films.find((film) => film.docstring === value);
       if (selectedFilm) {
         setCode(selectedFilm.code);
-      }
-      else{
-        setCode('')
+      } else {
+        setCode("");
       }
 
       setPreviousSearches((prevSearches) => [...prevSearches, value]);
@@ -37,15 +37,48 @@ export const QuerySearch: React.FC = () => {
     reason: AutocompleteInputChangeReason
   ) => {
     const input = value.toLowerCase();
-    const filteredSuggestions = top100Films
-      .map((option) => option.docstring)
-      .filter((docstring) => docstring.toLowerCase().includes(input));
-    setSuggestions(filteredSuggestions);
+    const filteredResults = top100Films.filter((film) =>
+      film.docstring.toLowerCase().includes(input)
+    );
+    const matchingStrings = filteredResults.map((film) => film.docstring);
+    const matchingCode = filteredResults.map((film) => film.code);
+    setSuggestions(matchingStrings);
+    setCode(matchingCode.join("\n")); // Combine relevant code into a single paragraph
   };
+
+
+
+// setting a log of the json file 
+const [data, setData] = useState<any[]>([]);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("bscs-2019-2023\src\content\test1.jsonl");
+      const content = await response.text();
+      const lines = content.split("\n");
+      const dataArray = lines.map((line) => {
+        const jsonData = JSON.parse(line);
+        console.log(jsonData, 'file'); // Log the parsed JSON object
+        return jsonData;
+      });
+      setData(dataArray);
+    } catch (error) {
+      console.error("Error fetching JSONL file:", error);
+    }
+  };
+const test = "bscs-2019-2023\src\content\test1.jsonl"
+  console.log('----',  test);
+  
+  fetchData();
+}, []);
+
+
+
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
-      {/* search aera */}
+      {/* search area */}
       <div
         style={{
           display: "flex",
@@ -75,10 +108,10 @@ export const QuerySearch: React.FC = () => {
           />
         </div>
       </div>
-      {/* show previos search */}
+      {/* show previous search */}
       <div>
-      {selectedItem && (
-        <>
+        {selectedItem && (
+          <>
             <b>History</b>
             <div
               style={{
@@ -88,48 +121,39 @@ export const QuerySearch: React.FC = () => {
                 flexWrap: "wrap",
                 gap: "4px",
               }}
-              >
+            >
               {previousSearches.map((search, index) => (
-                <>
-                  <p
-                    key={index}
-                    style={{
-                      backgroundColor: "lightgreen",
-                      borderRadius: "16px",
-                      padding: 8,
-                      color: "green",
-                      margin: 3,
-                    }}
-                  >
-                    {search},
-                  </p>
-                </>
+                <p
+                  key={index}
+                  style={{
+                    backgroundColor: "lightgreen",
+                    borderRadius: "16px",
+                    padding: 8,
+                    color: "green",
+                    margin: 3,
+                  }}
+                >
+                  {search},
+                </p>
               ))}
             </div>
           </>
-        ) }
-      </div>
-      {/* show related code */}
-      <div>
-        {code && (
-          <>
-            <b>Code:</b>
-            <pre>{code}</pre>
-          </>
         )}
       </div>
-      {/* {selectedItem && (
-        <>
-          <p>Selected Item: &nbsp; {selectedItem}</p>
-          {code && (
-            <>
-              <p>Code:</p>
-              <pre>{code}</pre>
-            </>
-          )}
-        </>
-      ) } */}
 
-    </div>
-  );
+      {/* show matching strings, URL, and code */}
+      {code && (
+        <div>
+          <b>Matching Strings, URL, and Code:</b>
+          {suggestions.map((matchingString, index) => (
+            <div key={index}>
+              <p>{matchingString}</p>
+              <a href={top100Films[index].url}>{top100Films[index].url}</a>
+              <pre>{code[index]}</pre>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
--- src/content/seachResultRelated.tsx
+++ src/content/seachResultRelated.tsx
@@ -1,30 +1,54 @@
 export const top100Films = [
   {
-    title: "ExternalMerger.items",
-    repo: "apache/spark",
-    path: "python/pyspark/shuffle.py",
-    original_string: "def items(self):\n        \"\"\" Return all merged items as iterator \"\"\"\n        if not self.pdata and not self.spills:\n            return iter(self.data.items())\n        return self._external_items()",
-    language: "python",
-    code: " **********************def items(self):\n        \"\"\" Return all merged items as iterator \"\"\"\n        if not self.pdata and not self.spills:\n            return iter(self.data.items())\n        return self._external_items()",
-    code_tokens: ["def", "items", "(", "self", ")", ":", "if", "not", "self", ".", "pdata", "and", "not", "self", ".", "spills", ":", "return", "iter", "(", "self", ".", "data", ".", "items", "(", ")", ")", "return", "self", ".", "_external_items", "(", ")"],
-    docstring: "Return all merged items as iterator test 1",
-    docstring_tokens: ["Return", "all", "merged", "items", "as", "iterator"],
-    sha: "618d6bff71073c8c93501ab7392c3cc579730f0b",
-    url: "https://github.com/apache/spark/blob/618d6bff71073c8c93501ab7392c3cc579730f0b/python/pyspark/shuffle.py#L339-L343",
-    partition: "train"
+    code: "def get_vid_from_url(url):\n        \"\"\"Extracts video ID from URL.\n        \"\"\"\n        return match1(url, r'youtu\\.be\\/([^?\\/]+)') or \\\n          match1(url, r'youtube\\.com\\/embed\\/([^\\/]+)') or \\\n          match1(url, r'youtube\\.com\\/v\\/([^\\/]+)') or \\\n          match1(url, r'youtube\\.com\\/watch\\/([^\\/]+)') or \\\n          parse_query_param(url, 'v') or \\\n          parse_query_param(parse_query_param(url, 'u'), 'v')",
+    docstring: "Extracts video ID from URL.",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/youtube.py#L135-L143",
   },
   {
-    title: "ExternalMergebjjlgljr.items",
-    repo: "apache/spark",
-    path: "python/pyspark/shuffle.py",
-    original_string: "def items(self):\n        \"\"\" Return all merged items as iterator \"\"\"\n        if not self.pdata and not self.spills:\n            return iter(self.data.items())\n        return self._external_items()",
-    language: "python",
-    code: "---------------------------------------this is test def items(self):\n        \"\"\" Return all merged items as iterator \"\"\"\n        if not self.pdata and not self.spills:\n            return iter(self.data.items())\n        return self._external_items()",
-    code_tokens: ["def", "items", "(", "self", ")", ":", "if", "not", "self", ".", "pdata", "and", "not", "self", ".", "spills", ":", "return", "iter", "(", "self", ".", "data", ".", "items", "(", ")", ")", "return", "self", ".", "_external_items", "(", ")"],
-    docstring: "Return all merged items as iterator test 454654",
-    docstring_tokens: ["Return", "all", "merged", "items", "as", "iterator"],
-    sha: "618d6bff71073c8c93501ab7392c3cc579730f0b",
-    url: "https://github.com/apache/spark/blob/618d6bff71073c8c93501ab7392c3cc579730f0b/python/pyspark/shuffle.py#L339-L343",
-    partition: "train"
-  }
+    code: 'def sina_xml_to_url_list(xml_data):\n    """str->list\n    Convert XML to URL List.\n    From Biligrab.\n    """\n    rawurl = []\n    dom = parseString(xml_data)\n    for node in dom.getElementsByTagName(\'durl\'):\n        url = node.getElementsByTagName(\'url\')[0]\n        rawurl.append(url.childNodes[0].data)\n    return rawurl',
+    docstring: "str->list\n    Convert XML to URL List.\n    From Biligrab.",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/miomio.py#L41-L51",
+  },
+  {
+    code: 'def makeMimi(upid):\n    """From http://cdn37.atwikiimg.com/sitescript/pub/dksitescript/FC2.site.js\n    Also com.hps.util.fc2.FC2EncrptUtil.makeMimiLocal\n    L110"""\n    strSeed = "gGddgPfeaf_gzyr"\n    prehash = upid + "_" + strSeed\n    return md5(prehash.encode(\'utf-8\')).hexdigest()',
+    docstring:
+      "From http://cdn37.atwikiimg.com/sitescript/pub/dksitescript/FC2.site.js\n    Also com.hps.util.fc2.FC2EncrptUtil.makeMimiLocal\n    L110",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/fc2video.py#L11-L17",
+  },
+  {
+    code: "def fc2video_download(url, output_dir='.', merge=True, info_only=False, **kwargs):\n    \"\"\"wrapper\"\"\"\n    #'http://video.fc2.com/en/content/20151021bTVKnbEw'\n    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'\n    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'\n    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'\n    hostname = urlparse(url).hostname\n    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):\n        return False\n    upid = match1(url, r'.+\\/content\\/(\\\\w+)')\n\n    fc2video_download_by_upid(upid, output_dir, merge, info_only)",
+    docstring: "wrapper",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/fc2video.py#L46-L57",
+  },
+  {
+    code: "def dailymotion_download(url, output_dir='.', merge=True, info_only=False, *kwargs):\n    \"\"\"Downloads Dailymotion videos by URL.\n    \"\"\"\n\n    html = get_content(rebuilt_url(url))\n    info = json.loads(match1(html, r'qualities\":({.+?}),\"'))\n    title = match1(html, r'\"video_title\"\\s:\\s*\"([^\"]+)\"') or \\\n            match1(html, r'\"title\"\\s*:\\s*\"([^\"]+)\"')\n    title = unicodize(title)\n\n    for quality in ['1080','720','480','380','240','144','auto']:\n        try:\n            real_url = info[quality][1][\"url\"]\n            if real_url:\n                break\n        except KeyError:\n            pass\n\n    mime, ext, size = url_info(real_url)\n\n    print_info(site_info, title, mime, size)\n    if not info_only:\n        download_urls([real_url], title, ext, size, output_dir=output_dir, merge=merge)",
+    docstring: "Downloads Dailymotion videos by URL.",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/dailymotion.py#L13-L35",
+  },
+  {
+    code: 'def dictify(r,root=True):\n    """http://stackoverflow.com/a/30923963/2946714"""\n    if root:\n        return {r.tag : dictify(r, False)}\n    d=copy(r.attrib)\n    if r.text:\n        d["_text"]=r.text\n    for x in r.findall("./*"):\n        if x.tag not in d:\n            d[x.tag]=[]\n        d[x.tag].append(dictify(x,False))\n    return d',
+    docstring: "http://stackoverflow.com/a/30923963/2946714",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/ucas.py#L18-L29",
+  },
+  {
+    code: "def ucas_download_single(url, output_dir='.', merge=False, info_only=False, **kwargs):\n    '''video page'''\n    html = get_content(url)\n    # resourceID is UUID\n    resourceID = re.findall( r'resourceID\":\"([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', html)[0]\n    assert resourceID != '', 'Cannot find resourceID!'\n\n    title = match1(html, r'<div class=\"bc-h\">(.+)</div>')\n    url_lists = _ucas_get_url_lists_by_resourceID(resourceID)\n    assert url_lists, 'Cannot find any URL of such class!'\n    \n    for k, part in enumerate(url_lists):\n        part_title = title + '_' + str(k)\n        print_info(site_info, part_title, 'flv', 0)\n        if not info_only:\n            download_urls(part, part_title, 'flv', total_size=None, output_dir=output_dir, merge=merge)",
+    docstring: "video page",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/ucas.py#L102-L117",
+  },
+  {
+    code: "def ucas_download_playlist(url, output_dir='.', merge=False, info_only=False, **kwargs):\n    '''course page'''\n    html = get_content(url)\n\n    parts = re.findall( r'(getplaytitle.do\\?.+)\"', html)\n    assert parts, 'No part found!'\n\n    for part_path in parts:\n        ucas_download('http://v.ucas.ac.cn/course/' + part_path, output_dir=output_dir, merge=merge, info_only=info_only)",
+    docstring: "course page",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/ucas.py#L119-L127",
+  },
+  {
+    code: "def sina_download_by_vid(vid, title=None, output_dir='.', merge=True, info_only=False):\n    \"\"\"Downloads a Sina video by its unique vid.\n    http://video.sina.com.cn/\n    \"\"\"\n    xml = api_req(vid)\n    urls, name, size = video_info(xml)\n    if urls is None:\n        log.wtf(name)\n    title = name\n    print_info(site_info, title, 'flv', size)\n    if not info_only:\n        download_urls(urls, title, 'flv', size, output_dir = output_dir, merge = merge)",
+    docstring:
+      "Downloads a Sina video by its unique vid.\n    http://video.sina.com.cn/",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/sina.py#L41-L52",
+  },
+  {
+    code: "def fc2video_download(url, output_dir='.', merge=True, info_only=False, **kwargs):\n    \"\"\"Wrapper.\"\"\"\n    hostname = urlparse(url).hostname\n    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):\n        return False\n    upid = match1(url, r'.+/content/(\\w+)')\n\n    fc2video_download_by_upid(upid, output_dir, merge, info_only)",
+    docstring: "Wrapper.",
+    url: "https://github.com/soimort/you-get/blob/b746ac01c9f39de94cac2d56f665285b0523b974/src/you_get/extractors/fc2video.py#L19-L25",
+  },
 ];
